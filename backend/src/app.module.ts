6a2b65cf6bb11eb7c2fa756f20e36c7d21588fba
Huan-Yee <lim.huan.yee@student.mmu.edu.my>
2025-11-23 18:25:05 +0800

    Setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ParkingLotModule } from './parking_lot/parking_lot.module';
+import { ParkingLot } from './parking_lot/entities/parking_lot.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'rebon123',
+      database: 'mmu_parking_db',
+      entities: [ParkingLot],
+      synchronize: true,
+    }),
+    ParkingLotModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
